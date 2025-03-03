af201486637dd11e2fb6985a728b320a23161310
chris <chrisjfinnerty@gmail.com>
2025-03-02 18:40:47 -0800

    Added __filename and __dirname for routing

--- EchoCity/backend/app.js
+++ EchoCity/backend/app.js
@@ -20,6 +20,9 @@ const origin = process.env.NODE_ENV === 'production'
             : process.env.FRONTEND_URL || 'http://localhost:5173'
 console.log('CORS origin set to:', origin);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
 
 app.use(cookieParser());
@@ -39,11 +42,10 @@ app.use('/api/favorites', favoritesRoutes);
 app.use('/api/userEvents', userEvents);
 
 if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, 'client', 'dist')));
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+        res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
     });
 }
 
